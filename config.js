055345e630a4d2036a986e908ebbcacf2744fa02
jbarrosbaruoffshore-stack <jbarros.baruoffshore@gmail.com>
2026-07-05 02:09:04 -0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+window.BARBER_CONFIG = {
+  supabaseUrl: "https://abcpqhtggieyxpqbasny.supabase.co",
+  supabaseAnonKey: "sb_publishable_jtZkUGs7mU-IAsNAi2Eb9w_8m6SN1i_",
+  whatsappNumber: "5521998200135",
+  businessName: "Barbearia do FB",
+  demoAdminEmail: "fb@barbearia2026.com",
+  demoAdminPasswordHash: "ec9e7f5b208122954f4634565572959b043807755d47fe3681062fd6c26bacdd"
+};
